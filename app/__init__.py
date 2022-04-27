30f282ebde18dc55a0232a7bd97e5c406350ffa9
j-azuma <azumanotdetail@gmail.com>
2022-04-27 22:16:49 +0900

    基盤コード

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from flask import (Flask)
+
+def create_app():
+    
+    load_dotenv()
+    app = Flask(__name__, instance_relative_config=True)
+    
+    return app 
+
+
+app = create_app()
+    
