e7bb0bb5e6a4dd371587318d84da773338518b59
j-azuma <azumanotdetail@gmail.com>
2022-05-03 21:44:42 +0900

    User集約のDTO

--- app/configration/database/initdb.py
+++ app/configration/database/initdb.py
@@ -1,8 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
 
-# データベース接続設定を記述
-db = SQLAlchemy()
+# DB接続設定
+engine = create_engine(str(os.getenv("SQLALCHEMY_DATABASE_URI")), echo = True)
 
-# DB設定を初期化
-def init_db(app):
-    db.init_app(app)
+# session設定
+session_maker = sessionmaker(autocommit=False,
+                             autoflush=False,
+                             bind=engine)
+# session設定
+db_session  = scoped_session(session_maker)
+
+# セッション初期化 
+session = db_session
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    from app.user.userdto import UserDto
+    Base.metadata.create_all(bind=engine)
